cf9f0fa6973a2e85d22bcae65550d07f3895cc87
Mikatech <mikael.vallenet@epitech.eu>
2022-09-14 16:50:39 +0200

    init project

---
+++ my-app/src/Form.js
@@ -0,0 +1,15 @@
+import reCAPTCHA from "react-google-recaptcha"
+
+const Form = () =>{
+
+    return(
+        <form>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" className="input"/>
+            <reCAPTCHA theme={"dark"} sitekey={process.env.REACT_APP_SITE_KEY}/>
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default Form
